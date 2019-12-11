3a7c0d84684acbaece58b7ed28405f7803ee002f
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-11 18:40:22 +0530

    Added: Finding Luckiest and Unluckiest Day

--- GamblerSimulation/gamblersimulation.sh
+++ GamblerSimulation/gamblersimulation.sh
@@ -22,58 +22,57 @@ echo "Welcome to Gambler Simulation"
 function gamble()
 {
 
-for((day=0; day<20; day++))
-do
-	Cash=$STAKE
-
-	while [[ $Cash -lt $winLimit ]] && [[ $Cash -gt $lossLimit ]]
+	for((day=0; day<20; day++))
 	do
-		Outcome=$((RANDOM%2))
-		if [ $Outcome -eq 1 ]
-		then
-			Cash=$(($Cash+1))
-			win=$(($win+1))
-		else
-			Cash=$(($Cash-1))
-			loss=$(($loss-1))
-		fi
+		Cash=$STAKE
+
+		while [[ $Cash -lt $winLimit ]] && [[ $Cash -gt $lossLimit ]]
+		do
+			Outcome=$((RANDOM%2))
+			if [ $Outcome -eq 1 ]
+			then
+				Cash=$(($Cash+1))
+				win=$(($win+1))
+			else
+				Cash=$(($Cash-1))
+				loss=$(($loss-1))
+			fi
+		done
+
+	amount=$(( $Cash-$STAKE ))
+	echo $amount
+	totalAmount=$(($totalAmount+$amount))
+	totalArr[$day]=$totalAmount
+	diffArr[$day]=$amount" "$totalAmount
+
 	done
-amount=$(( $Cash-$STAKE ))
-echo $amount
-totalAmount=$(($totalAmount+$amount))
-totalArr[$day]=$totalAmount
-diffArr[$day]=$amount" "$totalAmount
 
-done
+	echo "total amount:" $totalAmount
 
-echo "total amount:" $totalAmount
-
-for((day=0; day<20; day++))
-do
-	echo  "Day$day" ${diffArr[$day]}
-done
+	for((day=0; day<20; day++))
+	do
+		echo  "Day$day" ${diffArr[$day]}
+	done
 
 }
 gamble
 
 function Luckiest()
 {
-
-for((day=0; day<20; day++))
-do
-	echo "Day$day" ${totalArr[$day]}
-done | sort -k2 -nr | awk 'NR==1 {print($1 " "$2)}'
+	for((day=0; day<20; day++))
+	do
+		echo "Day$day" ${totalArr[$day]}
+	done | sort -k2 -nr | awk 'NR==1 {print($1 " "$2)}'
 }
 Luckiest
 
 
 function UnLuckiest()
 {
-
-for((day=0; day<20; day++))
-do
+	for((day=0; day<20; day++))
+	do
         echo "Day$day" ${totalArr[$day]}
-done | sort -k2 -n | awk 'NR==1 {print($1 " "$2)}'
+	done | sort -k2 -n | awk 'NR==1 {print($1 " "$2)}'
 }
 UnLuckiest
 
